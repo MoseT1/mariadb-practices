0ce70af12e1fabb5237ef9f18fb510e135b5d418
Mose Kim <100126792+MoseKim1214@users.noreply.github.com>
2023-07-10 10:50:43 +0900

    DAO Pattern

--- jdbc-practice/src/main/java/test/DeleteTest02.java
+++ jdbc-practice/src/main/java/test/DeleteTest02.java
@@ -3,19 +3,22 @@ package test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class SelectTest02 {
+public class DeleteTest02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		list();
+		delete(9L);
+	
+		
 	}
-	public static void list() {
+	public static boolean delete(Long no) {
+		boolean result = false;
 		Connection conn = null;
+		Statement stmt = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		try {
 			//1.JDBC Driver Class 로딩
 			Class.forName("org.mariadb.jdbc.Driver");
@@ -26,20 +29,24 @@ public class SelectTest02 {
 		    conn = DriverManager.getConnection(url, "webdb", "webdb");
 			
 		    
-		    //4. SQL 실행  - 여기선 sql문에 ; 붙이면 안된다. 
-		    String sql = "select no, name" + " from dept" + " order by no desc";
+		  
 		    
-		    conn.prepareStatement(sql);
+		    //4. SQL 실행  - 여기선 sql문에 ; 붙이면 안된다.
+		    String sql = "delete from dept where no= ?";
 		    
-		    //select는 executeQuery이다.
-		   rs = pstmt.executeQuery(); // executeInsert도 있지만 업데이트로 해도 된다.
+		    pstmt = conn.prepareStatement(sql);
 		    
-		   // 5.결과처리
-		   while(rs.next()) {
-			   Long no = rs.getLong(1);  // 1번째 column가져옴
-			   String name = rs.getString(2);
-			   System.out.println(no + ":" + name);
-		   }
+		    pstmt.setLong(1, no);
+		    
+		 
+		    
+		    int count = pstmt.executeUpdate(); // executeInsert도 있지만 업데이트로 해도 된다.
+		    
+		    //5. 결과 처리
+//		    if (count ==1) {
+//		    	result = true;
+//		    }
+		    result = count ==1;
 		    
 		}catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
@@ -49,11 +56,8 @@ public class SelectTest02 {
 			
 			//6. 자원 정리
 			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
+				if(stmt != null) {
+					stmt.close();
 				}
 				if(conn != null) {
 					conn.close();
@@ -62,7 +66,8 @@ public class SelectTest02 {
 				e.printStackTrace();
 			}
 		}
-		
+		return result;
 
 	}
+
 }
--- jdbc-practice/src/main/java/test/UpdateTest02.java
+++ jdbc-practice/src/main/java/test/UpdateTest02.java
@@ -3,19 +3,19 @@ package test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class SelectTest02 {
+public class UpdateTest02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		list();
+		update(10L, "경영지원2");
 	}
-	public static void list() {
+	
+	public static boolean update(Long deptNo, String deptName) {
+		boolean result = false;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		try {
 			//1.JDBC Driver Class 로딩
 			Class.forName("org.mariadb.jdbc.Driver");
@@ -25,21 +25,23 @@ public class SelectTest02 {
 			
 		    conn = DriverManager.getConnection(url, "webdb", "webdb");
 			
+		    //3. Statement 생성, 쿼리 날리는 애
+		   
 		    
 		    //4. SQL 실행  - 여기선 sql문에 ; 붙이면 안된다. 
-		    String sql = "select no, name" + " from dept" + " order by no desc";
+		    String sql = "update dept set name= ? where no= ?";
 		    
-		    conn.prepareStatement(sql);
+		    pstmt = conn.prepareStatement(sql);
 		    
-		    //select는 executeQuery이다.
-		   rs = pstmt.executeQuery(); // executeInsert도 있지만 업데이트로 해도 된다.
+		    pstmt.setString(1, deptName);
+		    pstmt.setLong(2, deptNo);
+		    int count = pstmt.executeUpdate(); // executeInsert도 있지만 업데이트로 해도 된다.
 		    
-		   // 5.결과처리
-		   while(rs.next()) {
-			   Long no = rs.getLong(1);  // 1번째 column가져옴
-			   String name = rs.getString(2);
-			   System.out.println(no + ":" + name);
-		   }
+		    //5. 결과 처리
+//		    if (count ==1) {
+//		    	result = true;
+//		    }
+		    result = count ==1;
 		    
 		}catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
@@ -49,9 +51,6 @@ public class SelectTest02 {
 			
 			//6. 자원 정리
 			try {
-				if(rs != null) {
-					rs.close();
-				}
 				if(pstmt != null) {
 					pstmt.close();
 				}
@@ -62,7 +61,7 @@ public class SelectTest02 {
 				e.printStackTrace();
 			}
 		}
-		
+		return result;
 
 	}
 }
